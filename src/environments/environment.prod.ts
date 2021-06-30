2a32d9a5ce7ab88dce3a87a9dee1f0bc3f881746
NikaJamburia <nikajamburia@gmail.com>
2021-06-30 15:51:22 +0400

    config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  userContentApiUrl: "http://localhost:8080/api/",
-  userApiUrl: "http://0.0.0.0:5000/api/v1/",
+  userContentApiUrl: "http://34.71.212.226:8080/api/",
+  userApiUrl: "http://34.71.212.226:5000/api/v1/",
   firebaseConfig: {
     apiKey: "AIzaSyD24DhMzVpRtRirIJkdPb4DYw6VUmiAun0",
     authDomain: "wanderer-ui.firebaseapp.com",
